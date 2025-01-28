176d6ba84bf80ec9db41e9dfbc7797dabd40b267
Lorenzo Cesana <lorenzo.cesanarale@gmail.com>
2025-01-28 16:48:30 +0100

    exo 8

--- exercice8.php
+++ exercice8.php
@@ -8,3 +8,40 @@
  * Implémentez les getters et setters pour manipuler le tableau de notes.
  * En dessous de la classe, créer un objet et appeler les méthodes
  */
+
+ class Student {
+    protected array $grades;
+
+    public function __construct(array $grades)
+    {
+        $this->grades = $grades;
+    }
+
+    public function getGrades(): array
+    {
+        return $this->grades;
+    }
+
+    public function setGrades(array $grades): self
+    {
+        $this->grades = $grades;
+
+        return $this;
+    }
+
+    public function getAverage(): float 
+    {
+        $average = 0;
+        $nbr = 0;
+        $array = $this->getGrades();
+        foreach ($array as &$grade) {
+            $average += $grade;
+            $nbr ++;
+        }
+        return $average/$nbr;
+    }
+ }
+
+ $arrayGrades = array(1, 20);
+ $Lorenzo = new Student($arrayGrades);
+ echo $Lorenzo->getAverage();
