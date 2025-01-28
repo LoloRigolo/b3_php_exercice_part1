8432def0c3bfe3de2f07d57f7e8e9606ff59b863
Lorenzo Cesana <lorenzo.cesanarale@gmail.com>
2025-01-28 17:01:44 +0100

    exo 11

--- exercice11.php
+++ exercice11.php
@@ -10,3 +10,19 @@
  * Inclure uniquement dans le calcul de la moyenne les valeurs comprise 32 et 45
  * Appeler la classe en passant les valeurs en paramètre
 */
+class Stats {
+    public static function average(array $array, float $min, float $max): float
+    {
+        $filtered = array_filter($array, function($value) use ($min, $max) {
+            return $value >= $min && $value <= $max;
+        });
+
+        return array_sum($filtered) / count($filtered);
+    }
+}
+
+$bodyTemperatures = [36.5, 37.8, 36.2, 38.5, 20.4, 40.5, 62.4, 37.4];
+
+$averageTemperature = Stats::average($bodyTemperatures, 32, 45);
+
+echo "La moyenne : $averageTemperature";
